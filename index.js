0b23bcd41e4b819f33c7eae386e26cdb33ed37d0
Josiel Lopez <jdlopez114@gmail.com>
2022-03-28 00:06:54 -0400

    commit tree 7fdc7fd263e228713e340358e8e5faca16fd7e77

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+function submitData(name, email){
+
+const configObj = {
+    method: 'POST',
+    headers: {
+        'Content-Type' : 'application/json',
+        Accept: 'application/json'
+    },
+    body: JSON.stringify({
+        name : `${name}`,
+        email : `${email}`
+})
+}
+
+const body = document.querySelector('body')
+body.className = 'body'
+
+return fetch('http://localhost:3000/users', configObj)
+.then(response => response.json())
+.then(obj => body.append(obj.id))
+.catch(error => body.append(error.message))
+
+}
+
+// const renderUserData = data => 
+
+submitData('Peter', 'pparker@esu.edu');
